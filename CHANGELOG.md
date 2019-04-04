043bf6f61606b016c179f2dac2ecfa3edb1fde9c
Daniel Indlekofer <di3@gmx.net>
2019-04-04 19:59:38 +0200

    eslint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+* added eslint
+
 ## 1.0.0 (2019-03-26)
 
 * init
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ export default (obj, find, def) => {
   if (obj === null || typeof obj != 'object') return def;
   var v = obj;
   for (let i = 0, c = find.length; i < c; i++) {
-    if (typeof v[find[i]] == "undefined") return def;
+    if (typeof v[find[i]] == 'undefined') return def;
     else v = v[find[i]];
   }
   return v;
-}
+};
